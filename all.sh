12e3cdd020bfe5781549ccc0139d9dbaeb0337e7
rakslice <rakslice@gmail.com>
2023-01-18 22:11:06 -0800

    bring superconf in; perl built; add some missing stuff in all.sh

--- all.sh
+++ all.sh
@@ -8,3 +8,13 @@ sh make.sh
 sh bash1.sh
 sh patch.sh
 sh bzip2.sh
+
+# new things that maybe have order problems
+sh diffutils.sh
+sh gawk.sh
+sh gdb.sh
+
+sh grep.sh
+sh grep-2.5.4.sh
+
+sh perl.sh
---
+++ perl.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+set -x
+
+pkgname=perl-5.6.2
+make=/usr/local/bin/make
+
+. gcc-2.7.2.3.inc
+#. gcc-2.95.3.inc
+. binutils-2.9.1.inc
+PATH=/usr/local/bin:$PATH
+export PATH
+
+. common-start.inc
+
+        rm -f config.sh Policy.sh
+        /archive/bash Configure -de -Dcc=gcc -Dusedl=n -Dosvers="1.3.0" \
+	  -Dinstallprefix=$pkgdir
+        $make
+
+	# tests disabled due to not passing stuff holding up the build
+	# TODO: Actually look into these and triage for further fixes
+	#       or explicit bypass so we can at least run the expected
+	#	working tests
+        #$make test
+
+. $pkgscripts/common-end.inc
