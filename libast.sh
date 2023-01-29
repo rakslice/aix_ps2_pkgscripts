a3684d761062a2c3bdd3c78df3bceddc2938d1b2
rakslice <rakslice@gmail.com>
2023-01-28 22:40:53 -0800

    libast

---
+++ libast.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+set -x
+
+pkgname=libast-0.7
+make=/usr/local/bin/make
+
+. gcc-2.7.2.3.inc
+#. gcc-2.95.3.inc
+. binutils-2.9.1.inc
+
+PATH=/usr/local/bin:$PATH
+export PATH
+
+. common-start.inc
+
+#cp $pkgscripts/codefragments/memmove.h .
+
+/usr/local/bin/patch -p4 -i $patches/$pkgname.patch
+
+ENV= CONFIG_SHELL=/archive/bash2 SHELL=/archive/bash2 /archive/bash2 ./configure
+
+$make 
+
+prefixvar=DESTDIR
+
+. $pkgscripts/common-end.inc
