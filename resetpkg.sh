8f26437996625a3bf4ee4f74a832f21e3bc22ac4
rakslice <rakslice@gmail.com>
2023-01-29 01:08:00 -0800

    clear the .pkg file during a reset

--- resetpkg.sh
+++ resetpkg.sh
@@ -13,3 +13,7 @@ rm -rf "/u/root/src/$srcname"
 if [ -d "/u/root/pkg/$srcname" ]; then
   rm -rf "/u/root/pkg/$srcname"
 fi
+
+if [ -f "/u/root/pkg/$srcname.pkg" ]; then
+  rm "/u/root/pkg/$srcname.pkg"
+fi
